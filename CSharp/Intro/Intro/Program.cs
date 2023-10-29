c90d20da6591c53ac3fa6fef23d2864bd1d6a8c0
iremoruc <irmmorcc@gmail.com>
2023-10-29 16:37:20 +0300

    update intro

--- CSharp/Intro/Intro/Program.cs
+++ CSharp/Intro/Intro/Program.cs
@@ -1,7 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
+﻿
+// Asal sayı çözümü
 //int sayac = 0;
 //for (int sayi = 2; sayi <= 100; sayi++)
 //{
