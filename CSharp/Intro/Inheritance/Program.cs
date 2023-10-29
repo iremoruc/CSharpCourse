27954541a981209feb5f2ef6320a90b3679a4900
iremoruc <irmmorcc@gmail.com>
2023-10-29 16:15:55 +0300

    added inheritance solutions

---
+++ CSharp/Intro/Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kalıtım-Miras
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName="İrem"
+                },new Student
+                {
+                    FirstName="Emir"
+                },new Person
+                {
+                    FirstName="Nur"
+                }
+            };
+            foreach(var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+        }
+        // Bir nesneyi bir kere miras alabiliriz. Önce inheritance sonra interfaceler. interfaclerin tek başına bir anlamı yoktur.
+        class Person
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        class Customer : Person
+        {
+            public string City { get; set; }
+        }
+        class Student : Person
+        {
+            public string City { get; set; }
+        }
+
+    }
+}
