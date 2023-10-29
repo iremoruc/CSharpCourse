631c25d48a3ac5f6f1b971a3e59e1b22ef206086
iremoruc <irmmorcc@gmail.com>
2023-10-29 16:28:01 +0300

    added interface demo solution

---
+++ CSharp/Intro/InterfacesDemo/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach(var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+
+        }
+    }
+
+    internal class Worker : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+
+    }
+    interface IEat
+    {
+        void Eat();
+    }
+    interface ISalary
+    {
+        void Eat();
+    }
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+
+        class Worker : IWorker, IEat, ISalary
+        {
+            public void Eat()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Work()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class Robot : IWorker
+        {
+            public void Work()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
