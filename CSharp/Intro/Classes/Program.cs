9d5f31cc02381502d9c6d48b22f4248168cbfe0b
iremoruc <irmmorcc@gmail.com>
2023-10-29 16:11:40 +0300

    added classes solutions

---
+++ CSharp/Intro/Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustemerManager custoManager = new CustemerManager();
+            ProductManager productManager = new ProductManager();
+            custoManager.Add();
+            custoManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "İstanbul";
+            customer.FirstName = "İrem";
+            customer.LastName = "Oruç";
+            customer.Id= 1;
+
+            Customer customer2 = new Customer
+            {
+                Id = 1, City = "İstanbul", FirstName = "Emir", LastName = "Baş"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+        }   
+
+    }
+}
